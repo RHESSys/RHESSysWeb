d57c3749e962064a25202e3f55e3c1ec60048f91
ga <ga@terrahub-vm.(none)>
2013-04-16 16:07:23 -0400

    working version can pull a raster out of a grass dataset and display it

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from mezzanine.pages.models import Page, RichText
-from ga_resources.models import DataResource
 
 class GrassEnvironment(Page, RichText):
     database = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     map_set = models.CharField(max_length=255)
+    default_raster = models.CharField(max_length=255, null=True, blank=True)
