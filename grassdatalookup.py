1376abcf0ed6408a0611011263ce4c43c971a680
Jeff Heard <jefferson.r.heard@gmail.com>
2013-04-19 14:29:07 -0400

    made grassdatalookup a class and allow for passed-in grass context

--- grassdatalookup.py
+++ grassdatalookup.py
@@ -37,6 +37,7 @@ import os, sys
 import tempfile
 from collections import namedtuple
 from ctypes import *
+import importlib
 
 from osgeo import osr
 
@@ -44,228 +45,237 @@ import rhessystypes
 
 GRASSConfig = namedtuple('GRASSConfig', ['gisbase', 'dbase', 'location', 'mapset'], verbose=False)
 
-
-def getSpatialReferenceForGRASSDataset(grassConfig):
-    """ @brief Return the spatial reference for a GRASS mapset 
-
-        @param grassConfig GRASSConfig specifying the GRASS mapset
-        that contains 
-
-        @return osgeo.osr instance representing the
-        spatial reference
-    """
-    _setupGrassScriptingEnvironment(grassConfig)
-    import grass.script as grass
-    s_srs = osr.SpatialReference()
-    proj = grass.read_command('g.proj', flags='j')
-    s_srs.ImportFromProj4( proj )
-    return s_srs
-
-def getCoordinatesForFQPatchIDs(fqPatchIDs, grassConfig, patchMap, zoneMap, hillslopeMap):
-    """ @brief Get the geographic coordinates for a list of patches identified by
-        their fully qualified patch ID. The fully qualified patch ID is the combination 
-        of the patchID, zoneID, and hillslopeID.
-    
-        @param List of rhessystypes.FQPatchID objects representing a fully qualified 
-        patch ID
-        @param grassConfig GRASSConfig specifying the GRASS mapset that contains 
-        the patch, zone, and hillslope maps
-        @param patchMap String representing the name of the patch map
-        @param zoneMap String representing the name of the zone map 
-        @param hillslopeMap String representing the name of the hillslope map
+class GrassDataLookup(object): 
+    def __init__(self, grass_scripting=None, grass_lib=None, grass_config=None):
+        self.grass_config = grass_config
         
-        @return List of rhessysweb.types.CoordinatePair objects
+        if not grass_scripting:
+            self.g = self._setupGrassScriptingEnvironment()
+        else:
+            self.g = grass_scripting
+            
+        if not grass_lib:
+            self.grass_lowlevel = self._setupGrassEnvironment()
+
+        else:
+            self.grass_lowlevel = grass_lib 
         
-        @todo Handle non-grid patches, return the coordinates of their centroid
-    """
-    coords = list()
     
-    # Set up GRASS environment
-    _setupGrassEnvironment(grassConfig)
-    from grass.lib import gis as grass
-    grass.G_gisinit('')
+    def getSpatialReferenceForGRASSDataset(self):
+        """ @brief Return the spatial reference for a GRASS mapset 
     
-    # Get the window so we can conver row,col to easting,northing
-    window = grass.Cell_head()
-    grass.G_get_window(byref(window))
-    numRows = grass.G_window_rows()
-    numCols = grass.G_window_cols()
+            @param grassConfig GRASSConfig specifying the GRASS mapset
+            that contains 
     
-    # Open patch map
-    patch = grass.G_find_cell2(patchMap, '')
-    patch = c_char_p(patch).value
-    patchType = grass.G_raster_map_type(patchMap, patch)
-    if patchType == 0: 
-        ptype = POINTER(c_int) 
-    elif patchType == 1: 
-        ptype = POINTER(c_float) 
-    elif patchType == 2: 
-        ptype = POINTER(c_double) 
-    patchFd = grass.G_open_cell_old(patchMap, patch) 
-    patchRast = grass.G_allocate_raster_buf(patchType)     
-    patchRast = cast(c_void_p(patchRast), ptype) 
-    
-    # Open zone map
-    zone = grass.G_find_cell2(zoneMap, '')
-    zone = c_char_p(zone).value
-    zoneType = grass.G_raster_map_type(zoneMap, zone)
-    if zoneType == 0: 
-        ptype = POINTER(c_int) 
-    elif zoneType == 1: 
-        ptype = POINTER(c_float) 
-    elif zoneType == 2: 
-        ptype = POINTER(c_double) 
-    zoneFd = grass.G_open_cell_old(zoneMap, zone) 
-    zoneRast = grass.G_allocate_raster_buf(zoneType)     
-    zoneRast = cast(c_void_p(zoneRast), ptype) 
-    
-    # Open hill map
-    hill = grass.G_find_cell2(hillslopeMap, '')
-    hill = c_char_p(hill).value
-    hillType = grass.G_raster_map_type(hillslopeMap, hill)
-    if hillType == 0: 
-        ptype = POINTER(c_int) 
-    elif hillType == 1: 
-        ptype = POINTER(c_float) 
-    elif hillType == 2: 
-        ptype = POINTER(c_double) 
-    hillFd = grass.G_open_cell_old(hillslopeMap, hill) 
-    hillRast = grass.G_allocate_raster_buf(hillType)     
-    hillRast = cast(c_void_p(hillRast), ptype) 
-     
-    for row in range(numRows):
-        # Get current row for each dataset
-        grass.G_get_raster_row(patchFd, patchRast, row, patchType)
-        grass.G_get_raster_row(zoneFd, zoneRast, row, zoneType)
-        grass.G_get_raster_row(hillFd, hillRast, row, hillType)
-    
-        for col in range(numCols):
-            for fqPatchID in fqPatchIDs:
-                if patchRast[col] == fqPatchID.patchID and \
-                    zoneRast[col] == fqPatchID.zoneID and \
-                    hillRast[col] == fqPatchID.hillID:
-                    # Match found, get its coordinates
-                    easting = grass.G_col_to_easting(col + 0.5, byref(window))
-                    northing = grass.G_row_to_northing(row + 0.5, byref(window))
-                    coords.append(rhessystypes.getCoordinatePair(easting, northing))
-    
-    # Clean up
-    grass.G_close_cell(patchFd)
-    grass.G_free(patchRast)
-    grass.G_close_cell(zoneFd)
-    grass.G_free(zoneRast)
-    grass.G_close_cell(hillFd)
-    grass.G_free(hillRast)
-    
-    os.environ['GIS_LOCK'] = ''
-    os.unlink(os.environ['GISRC'])
-    
-    return coords
-
-
-def getFQPatchIDForCoordinates(coordinate, grassConfig, patchMap, zoneMap, hillslopeMap):
-    """ @brief Get the fully qualified ID of the patch located at a coordinate pair.
-        The fully qualified patch ID is the combination of the patchID, zoneID,
-        and hillslopeID.
-    
-        @param coordinate rhessystypes.CoordinatePair
-        @param grassConfig GRASSConfig specifying the GRASS mapset that contains 
-        the patch, zone, and hillslope maps
-        @param patchMap String representing the name of the patch map
-        @param zoneMap String representing the name of the zone map 
-        @param hillslopeMap String representing the name of the hillslope map
+            @return osgeo.osr instance representing the
+            spatial reference
+        """
         
-        @return FQPatchID
-    """
-    patchID = None
-    zoneID = None
-    hillID = None
+        s_srs = osr.SpatialReference()
+        proj = self.g.read_command('g.proj', flags='j')
+        s_srs.ImportFromProj4( proj )
+        return s_srs
     
-    # Set up GRASS environment
-    _setupGrassEnvironment(grassConfig)
-    from grass.lib import gis as grass
-    grass.G_gisinit('')
+    def getCoordinatesForFQPatchIDs(self, fqPatchIDs, patchMap, zoneMap, hillslopeMap):
+        """ @brief Get the geographic coordinates for a list of patches identified by
+            their fully qualified patch ID. The fully qualified patch ID is the combination 
+            of the patchID, zoneID, and hillslopeID.
+        
+            @param List of rhessystypes.FQPatchID objects representing a fully qualified 
+            patch ID
+            @param grassConfig GRASSConfig specifying the GRASS mapset that contains 
+            the patch, zone, and hillslope maps
+            @param patchMap String representing the name of the patch map
+            @param zoneMap String representing the name of the zone map 
+            @param hillslopeMap String representing the name of the hillslope map
+            
+            @return List of rhessysweb.types.CoordinatePair objects
+            
+            @todo Handle non-grid patches, return the coordinates of their centroid
+        """
+        coords = list()
+        
+        # Set up GRASS environment
+        self.grass_lowlevel.G_gisinit('')
+        
+        # Get the window so we can conver row,col to easting,northing
+        window = self.grass_lowlevel.Cell_head()
+        self.grass_lowlevel.G_get_window(byref(window))
+        numRows = self.grass_lowlevel.G_window_rows()
+        numCols = self.grass_lowlevel.G_window_cols()
+        
+        # Open patch map
+        patch = self.grass_lowlevel.G_find_cell2(patchMap, '')
+        patch = c_char_p(patch).value
+        patchType = self.grass_lowlevel.G_raster_map_type(patchMap, patch)
+        if patchType == 0: 
+            ptype = POINTER(c_int) 
+        elif patchType == 1: 
+            ptype = POINTER(c_float) 
+        elif patchType == 2: 
+            ptype = POINTER(c_double) 
+        patchFd = self.grass_lowlevel.G_open_cell_old(patchMap, patch) 
+        patchRast = self.grass_lowlevel.G_allocate_raster_buf(patchType)     
+        patchRast = cast(c_void_p(patchRast), ptype) 
+        
+        # Open zone map
+        zone = self.grass_lowlevel.G_find_cell2(zoneMap, '')
+        zone = c_char_p(zone).value
+        zoneType = self.grass_lowlevel.G_raster_map_type(zoneMap, zone)
+        if zoneType == 0: 
+            ptype = POINTER(c_int) 
+        elif zoneType == 1: 
+            ptype = POINTER(c_float) 
+        elif zoneType == 2: 
+            ptype = POINTER(c_double) 
+        zoneFd = self.grass_lowlevel.G_open_cell_old(zoneMap, zone) 
+        zoneRast = self.grass_lowlevel.G_allocate_raster_buf(zoneType)     
+        zoneRast = cast(c_void_p(zoneRast), ptype) 
+        
+        # Open hill map
+        hill = self.grass_lowlevel.G_find_cell2(hillslopeMap, '')
+        hill = c_char_p(hill).value
+        hillType = self.grass_lowlevel.G_raster_map_type(hillslopeMap, hill)
+        if hillType == 0: 
+            ptype = POINTER(c_int) 
+        elif hillType == 1: 
+            ptype = POINTER(c_float) 
+        elif hillType == 2: 
+            ptype = POINTER(c_double) 
+        hillFd = self.grass_lowlevel.G_open_cell_old(hillslopeMap, hill) 
+        hillRast = self.grass_lowlevel.G_allocate_raster_buf(hillType)     
+        hillRast = cast(c_void_p(hillRast), ptype) 
+         
+        for row in range(numRows):
+            # Get current row for each dataset
+            self.grass_lowlevel.G_get_raster_row(patchFd, patchRast, row, patchType)
+            self.grass_lowlevel.G_get_raster_row(zoneFd, zoneRast, row, zoneType)
+            self.grass_lowlevel.G_get_raster_row(hillFd, hillRast, row, hillType)
+        
+            for col in range(numCols):
+                for fqPatchID in fqPatchIDs:
+                    if patchRast[col] == fqPatchID.patchID and \
+                        zoneRast[col] == fqPatchID.zoneID and \
+                        hillRast[col] == fqPatchID.hillID:
+                        # Match found, get its coordinates
+                        easting = self.grass_lowlevel.G_col_to_easting(col + 0.5, byref(window))
+                        northing = self.grass_lowlevel.G_row_to_northing(row + 0.5, byref(window))
+                        coords.append(rhessystypes.getCoordinatePair(easting, northing))
+        
+        # Clean up
+        self.grass_lowlevel.G_close_cell(patchFd)
+        self.grass_lowlevel.G_free(patchRast)
+        self.grass_lowlevel.G_close_cell(zoneFd)
+        self.grass_lowlevel.G_free(zoneRast)
+        self.grass_lowlevel.G_close_cell(hillFd)
+        self.grass_lowlevel.G_free(hillRast)
+        
+        os.environ['GIS_LOCK'] = ''
+        os.unlink(os.environ['GISRC'])
+        
+        return coords
     
-    # Translate coordinates to row, col
-    window = grass.Cell_head()
-    grass.G_get_window(byref(window))
-    row = int( grass.G_northing_to_row(coordinate.northing, byref(window)) )
-    col = int( grass.G_easting_to_col(coordinate.easting, byref(window)) )
-    #print("row: %d, col: %d\n" % (row, col) )
     
-    # Get number of rows
-    numRows = grass.G_window_rows()
-    numCols = grass.G_window_cols()
-    #print("num rows: %d, num cols: %d\n" % (numRows, numCols) )
+    def getFQPatchIDForCoordinates(self, coordinate, patchMap, zoneMap, hillslopeMap):
+        """ @brief Get the fully qualified ID of the patch located at a coordinate pair.
+            The fully qualified patch ID is the combination of the patchID, zoneID,
+            and hillslopeID.
+        
+            @param coordinate rhessystypes.CoordinatePair
+            @param grassConfig GRASSConfig specifying the GRASS mapset that contains 
+            the patch, zone, and hillslope maps
+            @param patchMap String representing the name of the patch map
+            @param zoneMap String representing the name of the zone map 
+            @param hillslopeMap String representing the name of the hillslope map
+            
+            @return FQPatchID
+        """
+        patchID = None
+        zoneID = None
+        hillID = None
+        
+        # Translate coordinates to row, col
+        window = self.grass_lowlevel.Cell_head()
+        self.grass_lowlevel.G_get_window(byref(window))
+        row = int( self.grass_lowlevel.G_northing_to_row(coordinate.northing, byref(window)) )
+        col = int( self.grass_lowlevel.G_easting_to_col(coordinate.easting, byref(window)) )
+        #print("row: %d, col: %d\n" % (row, col) )
+        
+        # Get number of rows
+        numRows = self.grass_lowlevel.G_window_rows()
+        numCols = self.grass_lowlevel.G_window_cols()
+        #print("num rows: %d, num cols: %d\n" % (numRows, numCols) )
+        
+        # Get patch ID
+        patchID = self._getValueForCell(patchMap, row, col)
+        #print("PatchID: %d\n" % (patchID,) )
+        
+        # Get zone ID
+        zoneID = self._getValueForCell(zoneMap, row, col)
+        #print("ZoneID: %d\n" % (zoneID,) )
+        
+        # Get hillslope ID
+        hillID = self._getValueForCell(hillslopeMap, row, col)
+        #print("HillID: %d\n" % (hillID,) )
+        
+        os.environ['GIS_LOCK'] = ''
+        os.unlink(os.environ['GISRC'])
+        
+        return rhessystypes.FQPatchID(patchID=int(patchID), zoneID=int(zoneID), hillID=int(hillID))
     
-    # Get patch ID
-    patchID = _getValueForCell(patchMap, row, col)
-    #print("PatchID: %d\n" % (patchID,) )
     
-    # Get zone ID
-    zoneID = _getValueForCell(zoneMap, row, col)
-    #print("ZoneID: %d\n" % (zoneID,) )
-    
-    # Get hillslope ID
-    hillID = _getValueForCell(hillslopeMap, row, col)
-    #print("HillID: %d\n" % (hillID,) )
-    
-    os.environ['GIS_LOCK'] = ''
-    os.unlink(os.environ['GISRC'])
-    
-    return rhessystypes.FQPatchID(patchID=int(patchID), zoneID=int(zoneID), hillID=int(hillID))
+    def _setupGrassScriptingEnvironment(self):
+        """ @brief Set up GRASS environment for using GRASS scripting API from 
+            Python (e.g. grass.script)
+        """
+        os.environ['GISBASE'] = self.grass_config.gisbase
+        sys.path.append(os.path.join(self.grass_config.gisbase, 'etc', 'python'))
+        import grass.script.setup as gsetup
+        gsetup.init(self.grass_config.gisbase, \
+                    self.grass_config.dbase, self.grass_config.location, \
+                    self.grass_config.mapset)
 
-
-def _setupGrassScriptingEnvironment(grassConfig):
-    """ @brief Set up GRASS environment for using GRASS scripting API from 
-        Python (e.g. grass.script)
-    """
-    os.environ['GISBASE'] = grassConfig.gisbase
-    sys.path.append(os.path.join(grassConfig.gisbase, 'etc', 'python'))
-    #os.environ['LD_LIBRARY_PATH'] = os.path.join(grassConfig.gisbase, 'lib')
-    #os.environ['DYLD_LIBRARY_PATH'] = os.path.join(grassConfig.gisbase, 'lib')
-    import grass.script.setup as gsetup
-    gsetup.init(grassConfig.gisbase, \
-                grassConfig.dbase, grassConfig.location, \
-                grassConfig.mapset)
-
-
-def _setupGrassEnvironment(grassConfig):
-    """ @brief Set up GRASS environment for using GRASS low-level API from 
-        Python (e.g. grass.lib)
-    """
-    gisBase = grassConfig.gisbase
-    sys.path.append(os.path.join(gisBase, 'etc', 'python'))
-    os.environ['LD_LIBRARY_PATH'] = os.path.join(gisBase, 'lib')
-    os.environ['DYLD_LIBRARY_PATH'] = os.path.join(gisBase, 'lib')
-    # Write grassrc
-    os.environ['GISRC'] = _initializeGrassrc(grassConfig)
-    os.environ['GIS_LOCK'] = str(os.getpid()) 
-
-def _initializeGrassrc(grassConfig):
-    grassRcFile = tempfile.NamedTemporaryFile(prefix='grassrc-', delete=False)
-    grassRcContent = "GISDBASE: %s\nLOCATION_NAME: %s\nMAPSET: %s\n" % \
-        (grassConfig.dbase, grassConfig.location, grassConfig.mapset)
-    grassRcFile.write(grassRcContent)
-    return grassRcFile.name
-
-def _getValueForCell(input, row, col):
-    from grass.lib import gis as grass
-    mapset = grass.G_find_cell2(input, '')
-    mapset = c_char_p(mapset).value
-    data_type = grass.G_raster_map_type(input, mapset)
-    if data_type == 0: 
-        ptype = POINTER(c_int) 
-    elif data_type == 1: 
-        ptype = POINTER(c_float) 
-    elif data_type == 2: 
-        ptype = POINTER(c_double) 
-    infd = grass.G_open_cell_old(input, mapset) 
-    inrast = grass.G_allocate_raster_buf(data_type)     
-    inrast = cast(c_void_p(inrast), ptype) 
-    grass.G_get_raster_row(infd, inrast, row, data_type)
-    value = inrast[col]
-    grass.G_close_cell(infd)
-    grass.G_free(inrast)
+        self.g = importlib.import_module('grass.script')
+        return self.g
     
-    return value
+    def _setupGrassEnvironment(self):
+        """ @brief Set up GRASS environment for using GRASS low-level API from 
+            Python (e.g. grass.lib)
+        """
+        gisBase = self.grass_config.gisbase
+        sys.path.append(os.path.join(gisBase, 'etc', 'python'))
+        os.environ['LD_LIBRARY_PATH'] = os.path.join(gisBase, 'lib')
+        os.environ['DYLD_LIBRARY_PATH'] = os.path.join(gisBase, 'lib')
+        # Write grassrc
+        os.environ['GISRC'] = self._initializeGrassrc()
+        os.environ['GIS_LOCK'] = str(os.getpid())
+        self.grass_lowlevel = importlib.import_module('grass.lib.gis')
+        self.grass_lowlevel.G_gisinit('')
+        return self.grass_lowlevel
+    
+    def _initializeGrassrc(self):
+        grassRcFile = tempfile.NamedTemporaryFile(prefix='grassrc-', delete=False)
+        grassRcContent = "GISDBASE: %s\nLOCATION_NAME: %s\nMAPSET: %s\n" % \
+            (self.grass_config.dbase, self.grass_config.location, self.grass_config.mapset)
+        grassRcFile.write(grassRcContent)
+        return grassRcFile.name
+    
+    def _getValueForCell(self, input, row, col):
+        mapset = self.grass_lowlevel.G_find_cell2(input, '')
+        mapset = c_char_p(mapset).value
+        data_type = self.grass_lowlevel.G_raster_map_type(input, mapset)
+        if data_type == 0: 
+            ptype = POINTER(c_int) 
+        elif data_type == 1: 
+            ptype = POINTER(c_float) 
+        elif data_type == 2: 
+            ptype = POINTER(c_double) 
+        infd = self.grass_lowlevel.G_open_cell_old(input, mapset) 
+        inrast = self.grass_lowlevel.G_allocate_raster_buf(data_type)     
+        inrast = cast(c_void_p(inrast), ptype) 
+        self.grass_lowlevel.G_get_raster_row(infd, inrast, row, data_type)
+        value = inrast[col]
+        self.grass_lowlevel.G_close_cell(infd)
+        self.grass_lowlevel.G_free(inrast)
+        
+        return value
